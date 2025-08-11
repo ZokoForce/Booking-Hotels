da809550250570b66b389e29dd0ec123ae346c51
ZokoForce <zokoforce@gmail.com>
2025-08-11 10:45:03 +0500

    Git ubdate 2

---
+++ Booking/Booking/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Booking;
+List<Room> rooms = new List<Room>();
+
+while (true)
+{
+    int command = int.Parse(Console.ReadLine());
+
+    if (command == 1)
+    {
+        int id = int.Parse(Console.ReadLine());
+        string type = Console.ReadLine();
+        int floor = int.Parse(Console.ReadLine());
+        int price = int.Parse(Console.ReadLine());
+        int capacity = int.Parse(Console.ReadLine());
+        string status = Console.ReadLine();
+        string description = Console.ReadLine();
+        Room room = new Room(id, type, floor, price, capacity, status, description);
+        rooms.Add(room);
+        Console.WriteLine("Room added successfully.");
+
+
+    }
+    else if (command == 2)
+    {
+        foreach (Room room in rooms)
+        {
+            room.GETInfo();
+        }
+    }
+}
---
+++ Booking/Booking/Room.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking
+{
+    internal class Room
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+        public int Floor { get; set; }
+        public int Price { get; set; }
+        public int Capacity { get; set; }
+        public string Status { get; set; }
+        public string Description { get; set; }
+
+        public Room(int id, string type, int floor, int price, int capacity, string status, string description)
+        {
+            ID = id;
+            Type = type;
+            Floor = floor;
+            Price = price;
+            Capacity = capacity;
+            Status = status;
+            Description = description;
+        }
+
+        public void GETInfo()
+        {
+            Console.WriteLine($"Room ID: {ID}, Tipe: {Type}, Price: {Price}");
+        }
+    }
+}
